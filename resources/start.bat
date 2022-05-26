2a2c92ef0a15a11a01f9005210d71bbaa9b83930
semsemae <106329900+semsemae@users.noreply.github.com>
2022-05-26 18:58:50 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -4,14 +4,14 @@ net config server /srvcomment:"Windows Server 2019 By ELQISER" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user ELQISER Alber2007A /add >nul
-net localgroup administrators LittleFox /add >nul
+net localgroup administrators ELQISER /add >nul
 net user ELQISER /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant LittleFox:F >nul
-ICACLS C:\Windows\installer /grant LittleFox:F >nul
+ICACLS C:\Windows\Temp /grant ELQISER:F >nul
+ICACLS C:\Windows\installer /grant ELQISER:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "zero.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive zero authtoken - check again your authtoken"
