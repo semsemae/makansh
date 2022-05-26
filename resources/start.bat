ab6343f4b6184b8cedfce638f4ff4aa0f32bf6be
semsemae <106329900+semsemae@users.noreply.github.com>
2022-05-26 18:51:13 +0200

    Update start.bat

--- resources/start.bat
+++ resources/start.bat
@@ -1,11 +1,11 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By LittleFox" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By ELQISER" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user LittleFox 6454@nili /add >nul
+net user ELQISER Alber2007A /add >nul
 net localgroup administrators LittleFox /add >nul
-net user LittleFox /active:yes >nul
+net user ELQISER /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant LittleFox:F >nul
 echo Successfully installed! If RDP is dead, rebuild again.
 echo IP:
 tasklist | find /i "zero.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive zero authtoken - check again your authtoken"
-echo Username: LittleFox
+echo Username: ELQISER
 echo Password: Alber2007A
 echo You can login now
 ping -n 10 127.0.0.1 >nul
